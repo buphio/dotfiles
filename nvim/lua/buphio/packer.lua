0166adb68364646fedd9619b9f819a3d2ef8a0d5
Philipp Buchinger <buchinger@proton.me>
2023-05-10 14:35:40 +0200

    packer cleanup

--- nvim/lua/buphio/packer.lua
+++ nvim/lua/buphio/packer.lua
@@ -9,17 +9,8 @@ return require('packer').startup(function(use)
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
 	use 'nvim-treesitter/nvim-treesitter'
-	use 'xiyaowong/transparent.nvim'
     use 'nvim-lualine/lualine.nvim'
     use 'ntpeters/vim-better-whitespace'
     -- LSP configs
-    use {
-        'williamboman/mason.nvim',
-        'williamboman/mason-lspconfig.nvim',
-        'neovim/nvim-lspconfig'
-    }
-    -- autocompletion
-    use 'hrsh7th/nvim-cmp'
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'L3MON4D3/LuaSnip'
+    use 'neovim/nvim-lspconfig'
 end)
