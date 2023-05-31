956298652d9b39d0490fc3ee3fa4b85953f0b437
Philipp Buchinger <buchinger@proton.me>
2023-05-31 21:28:50 +0200

    disabled lualine

--- nvim/lua/buphio/packer.lua
+++ nvim/lua/buphio/packer.lua
@@ -3,13 +3,15 @@ return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
     -- Color schemes
     use ({ 'rose-pine/neovim', as = 'rose-pine' })
+    use 'dikiaap/minimalist'
+    use 'projekt0n/github-nvim-theme'
     -- Misc tools
 	use {
 		'nvim-telescope/telescope.nvim', tag = '0.1.1',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
 	use 'nvim-treesitter/nvim-treesitter'
-    use 'nvim-lualine/lualine.nvim'
+    -- use 'nvim-lualine/lualine.nvim'
     use 'tpope/vim-fugitive'
     use 'ntpeters/vim-better-whitespace'
     use 'xiyaowong/transparent.nvim'
