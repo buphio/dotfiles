4a81a5439e0263a8448d2dda9fbaae000729a2a2
Philipp Buchinger <buchinger@proton.me>
2024-02-15 10:25:51 +0100

    initial commit

---
+++ .config/nvim/lua/buchinger/globals.lua
@@ -0,0 +1,16 @@
+-- lines
+vim.opt.relativenumber = true
+vim.opt.number = true
+--vim.opt.colorcolumn = "90"
+vim.opt.scrolloff = 8
+--vim.opt.signcolumn = "yes"
+vim.opt.wrap = false
+
+-- tabstops / spacing
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.smartindent = true
---
+++ .zshrc
@@ -0,0 +1,8 @@
+# Init starship
+eval "$(starship init zsh)"
+
+# Aliases
+alias ls="ls -G"
+alias ll="ls -l"
+alias la="ll -a"
+alias vi=nvim
